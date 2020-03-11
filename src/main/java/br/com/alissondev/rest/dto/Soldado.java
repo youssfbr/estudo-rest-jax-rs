8d08cc18cf2fa46136351da74ee4bd7aba708092
Alisson Youssf <youssfbr@gmail.com>
2020-03-11 00:23:46 -0300

    Criando classe Soldado

---
+++ src/main/java/br/com/alissondev/rest/dto/Soldado.java
@@ -0,0 +1,31 @@
+package br.com.alissondev.rest.dto;
+
+public class Soldado {
+    private String nome;
+    private String raca;
+    private String arma;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getArma() {
+        return arma;
+    }
+
+    public void setArma(String arma) {
+        this.arma = arma;
+    }
+}
